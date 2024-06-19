48f48c82b17d9de0d8e0ef2612e2c9fa06e81cea
mmorrii <byryshka55@gmail.com>
2024-06-19 22:43:40 +0300

    changed TablePanel.tsx

--- src/components/table/TablePanel.tsx
+++ src/components/table/TablePanel.tsx
@@ -1,48 +1,16 @@
-import {useContext, useEffect, useMemo, useState} from "react";
+import {useContext} from "react";
 import {HttpDataContext, WsDataContext} from "../../App.tsx";
 import {MemoryCell} from "./cells/MemoryCell.tsx";
 import {Table} from "./styles/tablePanel.ts";
 import {LineChartWithoutLegend} from "../charts/LineChartWithoutLegend.tsx";
-import {ContainerType, CoordsType} from "../../types/types.ts";
-import initialLineChartData from "../charts-data/lineChartData.json";
 
-const headTableData: string[] = ["Name", "Pids", "Image", "CPU usage", "Memory usage"]
-
-const getCurrentTime = () => new Date().getTime()
-
-const cpuUsage = (data: ContainerType): number => {
-    const cpu_delta = data.cpu_stats.cpu_usage.total_usage - data.precpu_stats.cpu_usage.total_usage
-    const system_cpu_delta = (data.cpu_stats.system_cpu_usage ?? 0) - (data.precpu_stats.system_cpu_usage ?? 0)
-    const number_cpus = data.cpu_stats.online_cpus ?? 0
-    const CPU_usage = (cpu_delta / system_cpu_delta) * number_cpus * 100
-
-    return CPU_usage === 0 ? 0 : Number(CPU_usage.toFixed(2))
-}
+const headTableData: string[] = ["Name", "Pids", "Status", "Image", "CPU usage", "Memory usage"]
 
 export const TablePanel = () => {
     console.log("RENDER TablePanel COMPONENT")
 
     const wsResponseData = useContext(WsDataContext)
     const httpResponseData = useContext(HttpDataContext)
-
-    const [chartData, setChartData] = useState<CoordsType[]>(initialLineChartData)
-
-    useEffect(() => {
-        if (httpResponseData.length > 0) {
-            httpResponseData.forEach(httpData => {
-                wsResponseData[httpData.Names[0]].slice(-1).forEach((data) => {
-                    if (data.cpu_stats.online_cpus) {
-                        setChartData(prevState => [
-                            ...prevState,
-                            {x: getCurrentTime(), y: cpuUsage(data)}
-                        ].slice(-20))
-                    }
-                })
-            })
-        }
-    }, [httpResponseData, wsResponseData])
-
-    const memoizedChartData = useMemo(() => chartData, [chartData]);
 
     return (
         <Table>
@@ -55,17 +23,17 @@ export const TablePanel = () => {
             </thead>
             <tbody>
                 { httpResponseData.map(httpData => (
-                    httpData.State === "running" &&
                     wsResponseData[httpData.Names[0]].slice(-1).map((data) => (
                         <tr key={data.keyId}>
                             <td>{httpData.Names[0]}</td>
                             <td>{data.pids_stats.current ?? "null"}</td>
+                            <td>status</td>
                             <td>
                                 <span title={httpData.Image}>{httpData.Image}</span>
                             </td>
                             <td>{ data.cpu_stats.online_cpus ?
-                                <div style={{ height: "46px", width: "140px"}}>
-                                    <LineChartWithoutLegend chartData={memoizedChartData} />
+                                <div style={{ height: "46px", width: "140px" }}>
+                                    <LineChartWithoutLegend containerName={data.name}/>
                                 </div> :
                                 "null"}
                             </td>
